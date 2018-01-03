3eb4d3dab29ff579f9b6f38e18558d2124cdf0ff
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-01-03 22:14:20 +0700

    Connecting the models to the admin

--- publications/admin.py
+++ publications/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    """
+    ModelAdmin class that
+    is going to control how we display the Article object
+    on the admin
+    """
+    pass
+
+
+@admin.register(models.Category)
+    """
+    ModelAdmin class that
+    is going to control how we display the Category object
+    on the admin
+    """
+    pass
+
+
+@admin.register(models.Magazine)
+    """
+    ModelAdmin class that
+    is going to control how we display the Magazine object
+    on the admin
+    """
+    pass
--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,9 @@ from . import models
 
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
+    """
+    ModelAdmin class that
+    is going to control how we display the User object
+    on the admin
+    """
     pass
